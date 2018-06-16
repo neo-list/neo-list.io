49bcab653642af2dac2662b52c968105e9dc66a0
tylors1 <tylors1@gmail.com>
2018-06-15 18:27:41 -0700

    set ratings

--- public/js/handlebars/helpers.js
+++ public/js/handlebars/helpers.js
@@ -14,3 +14,18 @@ Handlebars.registerHelper('trim', function(text) {
   text = text.replace(/ /g, '');
   return new Handlebars.SafeString(text);
 });
+
+Handlebars.registerHelper('rating', function(text) {
+  if (text === "0"){
+  	return new Handlebars.SafeString('rating-green');
+  }
+  else if (text === "1"){
+  	return new Handlebars.SafeString('rating-yellow');
+  }
+  else if (text === "2"){
+  	return new Handlebars.SafeString('rating-red');
+  }
+  else{
+  	return new Handlebars.SafeString('rating-none');
+  }
+});
