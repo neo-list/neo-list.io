5ca2236e1771840e02bc63ee4354e2b2d2cb8888
tylors1 <tylors1@gmail.com>
2018-06-15 21:43:55 -0700

    added tooltip

--- public/js/handlebars/upcoming.tmpl.js
+++ public/js/handlebars/upcoming.tmpl.js
@@ -5,7 +5,7 @@ templates['upcoming'] = template({"1":function(container,depth0,helpers,partials
 
   return ((stack1 = (helpers.equal || (depth0 && depth0.equal) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.status : depth0),"Upcoming",{"name":"equal","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"2":function(container,depth0,helpers,partials,data) {
-    var alias1=container.lambda, alias2=container.escapeExpression;
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<li id=\"project-line\" class=\"list-line w-clearfix\">\r\n    <div class=\"col gap\"></div>\r\n    <div class=\"col-info project w-clearfix\">\r\n      <div class=\"r1 w-row\">\r\n        <div class=\"col1 w-col w-col-5\"><a href=\"."
     + alias2(alias1((depth0 != null ? depth0.link : depth0), depth0))
@@ -25,25 +25,31 @@ templates['upcoming'] = template({"1":function(container,depth0,helpers,partials
     + alias2(alias1((depth0 != null ? depth0.goal : depth0), depth0))
     + "</div>\r\n    </div>\r\n    <div class=\"col info\">\r\n      <div class=\"images-row links w-row\">\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-kyc.svg\" width=\"14\" id=\""
     + alias2(alias1((depth0 != null ? depth0["project-id"] : depth0), depth0))
-    + "project-kyc\" tooltipster=\"top\" title=\""
-    + alias2(alias1((depth0 != null ? depth0.infokyc : depth0), depth0))
-    + "\" class=\"info-true\" ></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-whitelist.svg\" id=\""
+    + "project-kyc\" tooltipster=\"top\" "
+    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.infokyc : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + " ></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-whitelist.svg\" id=\""
     + alias2(alias1((depth0 != null ? depth0["project-id"] : depth0), depth0))
-    + "project-whitelist\" tooltipster=\"top\" title=\""
-    + alias2(alias1((depth0 != null ? depth0.infowhitelist : depth0), depth0))
-    + "\" class=\"info-true\"></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-location.svg\" id=\""
+    + "project-whitelist\" tooltipster=\"top\" "
+    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.infokyc : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + "></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-location.svg\" id=\""
     + alias2(alias1((depth0 != null ? depth0["project-id"] : depth0), depth0))
-    + "project-location\" tooltipster=\"top\" title=\""
-    + alias2(alias1((depth0 != null ? depth0.infolocations : depth0), depth0))
-    + "\" class=\"info-true\"></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-bonus.svg\" id=\""
+    + "project-location\" tooltipster=\"top\" "
+    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.infokyc : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + "></div>\r\n        <div class=\"images-col w-col w-col-3\"><img src=\"images/neolist-bonus.svg\" id=\""
     + alias2(alias1((depth0 != null ? depth0["project-id"] : depth0), depth0))
-    + "project-bonus\" tooltipster=\"top\" title=\""
-    + alias2(alias1((depth0 != null ? depth0.infobonus : depth0), depth0))
-    + "\" class=\"info-true\"></div>\r\n      </div>\r\n    </div>\r\n    <div class=\"col links\">\r\n      <div class=\"images-row w-row\">\r\n        <div class=\"images-col website w-col w-col-3\"><a href="
+    + "project-bonus\" tooltipster=\"top\" "
+    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.infokyc : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
+    + "></div>\r\n      </div>\r\n    </div>\r\n    <div class=\"col links\">\r\n      <div class=\"images-row w-row\">\r\n        <div class=\"images-col website w-col w-col-3\"><a href="
     + alias2(alias1((depth0 != null ? depth0.linkswebsite : depth0), depth0))
     + " id=\"website-link\" target=\"_blank\" class=\"w-inline-block\"><img src=\"images/neolist-website.svg\" id=\"project-website\" class=\"info-true\"></a></div>\r\n        <div class=\"images-col w-col w-col-3\"><a href="
     + alias2(alias1((depth0 != null ? depth0.linkswhitepaper : depth0), depth0))
     + " id=\"whitepaper-link\" target=\"_blank\" class=\"w-inline-block\"><img src=\"images/neolist-whitepaper.svg\" width=\"14\" id=\"project-whitepaper\" class=\"info-true\"></a></div>\r\n        <div class=\"images-col w-col w-col-3\"></div>\r\n        <div class=\"images-col w-col w-col-3\"></div>\r\n      </div>\r\n    </div>\r\n    </li>\r\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    return "title=\""
+    + container.escapeExpression(container.lambda((depth0 != null ? depth0.infokyc : depth0), depth0))
+    + "\"class=\"info-true\"";
+},"5":function(container,depth0,helpers,partials,data) {
+    return "title=\"None\"class=\"info-false\"";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
